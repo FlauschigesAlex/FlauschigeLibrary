621be2b039a846caa38898e7791cddf4d9b58254
FlauschigesAlex <git@flauschigesalex.at>
2026-05-15 04:01:13 +0200

    ✨ Improved ResourceManager handling in kotlin.

--- base/file/src/main/kotlin/at/flauschigesalex/lib/base/file/ResourceManager.kt
+++ base/file/src/main/kotlin/at/flauschigesalex/lib/base/file/ResourceManager.kt
@@ -8,30 +8,25 @@ import java.net.URL
 @Deprecated("Renamed to ResourceManager", ReplaceWith("ResourceManager"), DeprecationLevel.ERROR)
 typealias ResourceHandler = ResourceManager
 
-@Suppress("MemberVisibilityCanBePrivate")
+@Suppress("MemberVisibilityCanBePrivate", "DEPRECATION")
 class ResourceManager private constructor(val url: URL) : DataManager(url.toURI()) {
 
     companion object {
-        operator fun invoke(url: URL) : ResourceManager? {
-            try {
-                return ResourceManager(url)
-            } catch (_: Exception) {}
-            return null
-        }
+        operator fun invoke(url: URL) : ResourceManager? = runCatching {
+            ResourceManager(url)
+        }.getOrNull()
 
-        operator fun invoke(urlString: String) : ResourceManager? {
-            val url = this::class.java.classLoader.getResource(urlString) ?: return null
+        operator fun invoke(urlString: String, classLoader: ClassLoader? = null) : ResourceManager? {
+            val loader = classLoader ?: this::class.java.classLoader ?: return null
+            val url = loader.getResource(urlString) ?: return null
             return this(url)
         }
     }
 
-    override fun readStream(): InputStream? {
-        return try {
-            url.openStream()
-        } catch (_: Exception) {
-            null
-        }
-    }
+    @Deprecated("It's unlikely your action could not be performed with a bytearray.")
+    override fun readStream(): InputStream? = runCatching {
+        url.openStream()
+    }.getOrNull()
 
     override val isReadable: Boolean
         get() = this.readStream() != null
